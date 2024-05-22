6191a77877e3c5c443b885632d839fc583158bfb
Moe <mohamedmidani2001@gmail.com>
2024-05-22 11:02:22 -0400

    FirstExpandedView update

--- trofiApp/Views/FirstExpandedView.swift
+++ trofiApp/Views/FirstExpandedView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AppIntents
+import UIKit
 
 struct FirstExpandedView: View {
     
@@ -20,29 +21,37 @@ struct FirstExpandedView: View {
     @State private var notes: String = ""
     @State var rating: CGFloat
     var maxRating: Int
-
+    
+    @State private var showActionSheet = false
+    @State private var showImagePicker = false
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     
     
-
-
-
+    let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    
+    
     var body: some View {
         
         let stars = HStack(spacing: 0) {
-                    ForEach(0..<maxRating, id: \.self) { _ in
-                        Image(systemName: "star.square")
-                        
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 60, height: 50)
-                          
-                    }
-                }
-
+            ForEach(0..<maxRating, id: \.self) { _ in
+                Image(systemName: "star.square")
+                
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50, height: 40)
+                
+            }
+        }
+        
         
         ZStack {
             Color.gray0.ignoresSafeArea()
             ZStack(alignment: .bottom) {
+                
                 VStack(alignment: .leading, spacing: 16) {
                     HStack {
                         Text("Log Meal")
@@ -78,153 +87,234 @@ struct FirstExpandedView: View {
                                 .cornerRadius(32)
                         })
                     }
-                    Spacer()
+                    .padding(.top, 25)
+                    .padding()
+//                    Spacer()
+                    VStack(spacing: 16) {
+                        
+                        
+                        Button {
+                            print("photo button was tapped")
+                            showActionSheet = true
+                            
+                        } label: {
+                            ZStack{
+                                Rectangle()
+                                    .fill(Color.accentColor.opacity(0.1))
+                                    .cornerRadius(14)
+                                Image(systemName: "photo.badge.plus").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            }}
+                        .actionSheet(isPresented: $showActionSheet) {
+                            ActionSheet(title: Text("Select an option"), buttons: [
+                                .default(Text("Select from Gallery")) {
+                                    sourceType = .photoLibrary
+                                    showImagePicker = true
+                                },
+                                .default(Text("Camera")) {
+                                    sourceType = .camera
+                                    showImagePicker = true
+                                },
+                                .cancel()
+                            ])
+                        }
+                        .sheet(isPresented: $showImagePicker) {
+                            ImagePicker(sourceType: sourceType)
+                        }
+                        
+                        
+                        VStack{
+                            Spacer()
+                            Picker("", selection: $selection) {
+                                Text("Homecooked").tag(0)                        .foregroundColor(Color.accentColor)
+                                    .background(Color.accentColor)
+                                Text("Restaurant").tag(1)                        .foregroundColor(Color.accentColor)
+                                
+                            }
+                            .foregroundColor(Color.accentColor)
+                            .pickerStyle(.segmented)
+                            //                        .overlay(RoundedRectangle(cornerRadius: 15)
+                            //                                    .stroke(Color.gray, lineWidth: 4)
+                            //                            )
+                            .colorMultiply(Color.accentColor)
+                            .frame(width: 350)
+                            .padding(.bottom)
+                            
+                            TextField(
+                                "Title:",
+                                text: $title
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.accentColor, lineWidth: 2))
+                            .textFieldStyle(.roundedBorder)
+                            //                        .padding(.leading)
+                            //                        .padding(.trailing)
+                            //                        .padding(.bottom)
+                            .padding([.leading, .trailing, .bottom]) // Adjust the value as needed
+                            
+                            
+                            TextField(
+                                "Notes:",
+                                text: $notes
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.accentColor, lineWidth: 2))
+                            .textFieldStyle(.roundedBorder)
+                            .padding([.leading, .trailing, .bottom]) // Adjust the value as needed
+                            
+                            
+                            TextField(
+                                "Recipe:",
+                                text: $recipe
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.accentColor, lineWidth: 2))
+                            .textFieldStyle(.roundedBorder)
+                            .padding([.leading, .trailing, .bottom]) // Adjust the value as needed
+                            
+                            
+                            
+                            TextField(
+                                "Rating:",
+                                value: $rating, formatter: formatter
+                            )
+                            .keyboardType(.decimalPad)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.accentColor, lineWidth: 2))
+                            .textFieldStyle(.roundedBorder)
+                            .padding([.leading, .trailing, .bottom]) // Adjust the value as needed
+                            
+                            
+                            
+//                            Spacer()
+                            
+                            
+                            stars.overlay(
+                                GeometryReader { g in
+                                    let width = rating / CGFloat(maxRating) * g.size.width
+                                    ZStack(alignment: .leading) {
+                                        Rectangle()
+                                            .frame(width: width)
+                                            .foregroundColor(.accentColor)
+                                    }
+                                }
+                                    .mask(stars)
+                            )
+                            .foregroundColor(.gray)
+                        }
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.top, 5)
                     
-                    VStack{
-                        Spacer()
-                        Picker("", selection: $selection) {
-                            Text("Homecooked").tag(0)                        .foregroundColor(Color.accentColor)
-                                .background(Color.accentColor)
-                            Text("Restaurant").tag(1)                        .foregroundColor(Color.accentColor)
+                    
+                    
+                    Button(action: {
+                        //                    withAnimation(.spring(response: 0.25, dampingFraction: 1.2)) {
+                        ////                        viewModel.showItems = true
+                        //                        HapticManager.instance.impact(style: .light)
+                        //                        viewModel.selectedExpandIndex = nil
+                        //                    }
+                        
+                        withAnimation(.spring(response: 0.28, dampingFraction: 1.2)) {
+                            HapticManager.instance.impact(style: .light)
+                            viewModel.selectedExpandIndex = nil
+                            //
+                            
+                            //                                withAnimation(.spring(response: 0.88, dampingFraction: 0.9)) {
+                            //                                    viewModel.showItems = false
+                            //                                }
+                            viewModel.showItems = false
+                            viewModel.moveItems = false
                             
                         }
-                        .foregroundColor(Color.accentColor)
-                        .pickerStyle(.segmented)
-                        //                        .overlay(RoundedRectangle(cornerRadius: 15)
-                        //                                    .stroke(Color.gray, lineWidth: 4)
-                        //                            )
-                        .colorMultiply(Color.accentColor)
-                        .frame(width: 350)
                         
+                    } ,label: {
                         
-                        TextField(
-                            "Title:",
-                            text: $title
-                        )
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 14)
-                                .stroke(Color.accentColor, lineWidth: 2))
-                        .textFieldStyle(.roundedBorder)
-                        .padding()
+                        Rectangle()
                         
-                        TextField(
-                            "Notes:",
-                            text: $notes
-                        )
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 14)
-                                .stroke(Color.accentColor, lineWidth: 2))
-                        .textFieldStyle(.roundedBorder)
-                        .padding()
+                            .foregroundStyle(.accent)
                         
-                        TextField(
-                            "Recipe:",
-                            text: $recipe
-                        )
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 14)
-                                .stroke(Color.accentColor, lineWidth: 2))
-                        .textFieldStyle(.roundedBorder)
-                        .padding()
-                        
-//                        
-//                        TextField(
-//                            "Rating:",
-//                            text: $rating,
-//                            format: .float
-//                        )
-//                        .keyboardType(.decimalPad)
-//                        .overlay(
-//                            RoundedRectangle(cornerRadius: 14)
-//                                .stroke(Color.accentColor, lineWidth: 2))
-//                        .textFieldStyle(.roundedBorder)
-//                        .padding()
-//                        
-                        
-                        Spacer()
-                        
-                        
-                        stars.overlay(
-                                  GeometryReader { g in
-                                      let width = rating / CGFloat(maxRating) * g.size.width
-                                      ZStack(alignment: .leading) {
-                                          Rectangle()
-                                              .frame(width: width)
-                                              .foregroundColor(.accentColor)
-                                      }
-                                  }
-                                  .mask(stars)
-                              )
-                              .foregroundColor(.gray)
-                    }
+                            .frame(width: 300, height: 56, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .clipShape(RoundedRectangle(cornerRadius: 40, style: .continuous))
+                            .overlay(
+                                Text("Confirm")
+                                    .foregroundColor(.black) // Set the text color
+                            )
+                            .padding(48)
+                    })
+                    .buttonStyle(BouncyButton())
                 }
-                .padding(.horizontal, 20)
-                .padding(.bottom, 150)
-                .padding(.top, 80)
                 
-              
-
-                Button(action: {
-                    //                    withAnimation(.spring(response: 0.25, dampingFraction: 1.2)) {
-                    ////                        viewModel.showItems = true
-                    //                        HapticManager.instance.impact(style: .light)
-                    //                        viewModel.selectedExpandIndex = nil
-                    //                    }
-                    
-                    withAnimation(.spring(response: 0.28, dampingFraction: 1.2)) {
-                        HapticManager.instance.impact(style: .light)
-                        viewModel.selectedExpandIndex = nil
-                        //
-                        
-                        //                                withAnimation(.spring(response: 0.88, dampingFraction: 0.9)) {
-                        //                                    viewModel.showItems = false
-                        //                                }
-                        viewModel.showItems = false
-                        viewModel.moveItems = false
-                        
-                    }
-                    
-                } ,label: {
-                    
-                    Rectangle()
-                    
-                        .foregroundStyle(.accent)
-                    
-                        .frame(width: 300, height: 56, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .clipShape(RoundedRectangle(cornerRadius: 40, style: .continuous))
-                        .overlay(
-                            Text("Confirm")
-                                .foregroundColor(.black) // Set the text color
-                        )
-                        .padding(48)
-                })
-                .buttonStyle(BouncyButton())
+                .scaleEffect(appear ? 1 : 0.5, anchor: .bottom)
+                .opacity(appear ? 1 : 0)
+                .transition(.scale(scale: 1)) // this transition is kept to remove the default opacity fade when using a matchedGeometryEffect.
             }
+            .fontDesign(.rounded)
+            .background(
+                RoundedRectangle(cornerRadius: 24, style: .continuous)
+                    .fill(.gray0)
+                    .matchedGeometryEffect(id: "background", in: namespace)
+            )
+            .mask({
+                RoundedRectangle(cornerRadius: 24, style: .continuous)
+                    .matchedGeometryEffect(id: "mask", in: namespace)
+                
+            })
+            .ignoresSafeArea(.all)
             
-            .scaleEffect(appear ? 1 : 0.5, anchor: .bottom)
-            .opacity(appear ? 1 : 0)
-            .transition(.scale(scale: 1)) // this transition is kept to remove the default opacity fade when using a matchedGeometryEffect.
+            .onAppear(perform: {
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.85, blendDuration: 1)) {
+                    appear = true
+                }
+            })
         }
-        .fontDesign(.rounded)
-        .background(
-            RoundedRectangle(cornerRadius: 24, style: .continuous)
-                .fill(.gray0)
-                .matchedGeometryEffect(id: "background", in: namespace)
-        )
-        .mask({
-            RoundedRectangle(cornerRadius: 24, style: .continuous)
-                .matchedGeometryEffect(id: "mask", in: namespace)
-            
-        })
-        .ignoresSafeArea(.all)
-        
-        .onAppear(perform: {
-            withAnimation(.spring(response: 0.3, dampingFraction: 0.85, blendDuration: 1)) {
-                appear = true
-            }
-        })
     }
+    
+    
 }
+    
+    struct ImagePicker: UIViewControllerRepresentable {
+        var sourceType: UIImagePickerController.SourceType
+
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = context.coordinator
+            return picker
+        }
+
+        func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
+        func makeCoordinator() -> Coordinator {
+            Coordinator(self)
+        }
+
+        class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+            let parent: ImagePicker
+
+            init(_ parent: ImagePicker) {
+                self.parent = parent
+            }
+
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+                // Handle the selected image
+                picker.dismiss(animated: true)
+            }
+
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                picker.dismiss(animated: true)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
 
 struct FirstExpandedView_Preview: PreviewProvider {
     @Namespace static var namespace
