e3aea9259bc613bdddec025d489970d628d5693b
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2024-05-29 11:11:02 -0400

    someUpddae

--- trofiApp/Views/SecondExpandedView.swift
+++ trofiApp/Views/SecondExpandedView.swift
@@ -14,80 +14,178 @@ struct SecondExpandedView: View {
     @State private var appear: Bool = false
     @EnvironmentObject var viewModel: HomeViewModel
     
-    let columns: [GridItem] = [
-            GridItem(.flexible(), spacing: 16),
-            GridItem(.flexible(), spacing: 16)
-        ]
-    
-    var body: some View {
-        ZStack {
-            Color.gray900.ignoresSafeArea()
-            VStack(alignment: .center, spacing: 16) {
-                HStack {
-                    Text("Explore")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.gray0)
-                    Spacer()
-                    Button(action: {
-                        withAnimation(.spring(response: 0.25, dampingFraction: 1.24)) {
-                            viewModel.showItems = true
-                            HapticManager.instance.impact(style: .light)
-                            viewModel.selectedExpandIndex = nil
-                        }
-                        withAnimation(.spring(response: 0.28, dampingFraction: 1.2)) {
-                            viewModel.moveItems = false
-                        }
-                    }, label: {
-                        Image(systemName: "xmark")
-                            .renderingMode(.template)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.gray100)
-                            .padding(12)
-                            .background(.gray800)
-                            .cornerRadius(32)
-                })
-                }
-                
-                ScrollView(.vertical, showsIndicators: false) {
-                    LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(0..<12) { index in
-                            Rectangle()
-                                .foregroundStyle(.gray800.opacity(0.6))
-                                .frame(height: 150, alignment: .center)
-                                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                        }
-                    }
-                }
-                .scrollDisabled(false)
-            }
-            .padding(.horizontal, 20)
-            .padding(.bottom, 16)
-            .padding(.top, 80)
-            .scaleEffect(appear ? 1 : 0.5, anchor: .bottom)
-            .opacity(appear ? 1 : 0)
-            .transition(.scale(scale: 1)) // this transition is kept to remove the default opacity fade when using a matchedGeometryEffect.
+    @ObservedObject var networkManager = NetworkManager()
+        @State private var searchQuery = ""
 
-        }
-        .fontDesign(.rounded)
-        .background(
-            RoundedRectangle(cornerRadius: 24, style: .continuous)
-                .fill(.gray900)
-                .matchedGeometryEffect(id: "background", in: namespace)
-        )
-        .mask({
-            RoundedRectangle(cornerRadius: 24, style: .continuous)
-                .matchedGeometryEffect(id: "mask", in: namespace)
-            
-        })
-        .ignoresSafeArea(.all)
-        .onAppear(perform: {
-            withAnimation(.spring(response: 0.3, dampingFraction: 0.85, blendDuration: 1)) {
-                appear = true
-            }
-        })
+        var body: some View {
+            NavigationView {
+                
+                ZStack {
+                    
+                    Color.gray0.ignoresSafeArea()
+                    ZStack(alignment: .bottom) {
+                        
+                        VStack(alignment: .leading, spacing: 16) {
+                            
+                            HStack {
+                                Text("Recipe Search")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundStyle(.gray900)
+                                
+                                
+                                
+                                Spacer()
+                                Button(action: {
+                                    withAnimation(.spring(response: 0.45, dampingFraction: 1.2, blendDuration: 2)) {
+                                        viewModel.showItems = false
+                                        HapticManager.instance.impact(style: .light)
+                                        viewModel.selectedExpandIndex = nil
+                                    }
+                                    
+                                    //                            viewModel.moveItems = false
+                                    
+                                }, label: {
+                                    Image(systemName: "xmark")
+                                        .renderingMode(.template)
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.gray100)
+                                        .padding(12)
+                                        .background(Color.accentColor)
+                                        .cornerRadius(32)
+                                })
+                            }
+                            .padding(.top,60)
+                            .padding()
+                            VStack(spacing: 16) {
+                                
+                                VStack{
+                                    VStack {
+                                        TextField("Search for recipes", text: $searchQuery, onCommit: {
+                                            networkManager.fetchRecipes(query: searchQuery)
+                                        })
+                                        
+                                        //                                                    .padding()
+                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                        
+                                        List(networkManager.recipes) { recipe in
+                                            NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                                                HStack {
+                                                    
+                                                    AsyncImage(url: URL(string: recipe.image)) { image in
+                                                        image.resizable()
+                                                    } placeholder: {
+                                                        ProgressView()
+                                                    }
+                                                    .frame(width: 50, height: 50)
+                                                    .clipShape(RoundedRectangle(cornerRadius: 45))
+                                                    
+                                                    VStack(alignment: .leading) {
+                                                        Text(recipe.label)
+                                                            .font(.headline)
+                                                        Text(recipe.source)
+                                                            .font(.subheadline)
+                                                            .foregroundColor(.secondary)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    Spacer()
+                                    
+                                    
+                                }
+                            }
+                            .padding(.horizontal, 15)
+                            .padding(.top, 5)
+                            
+                            
+                            
+                            Button(action: {
+                                withAnimation(.spring(response: 0.28, dampingFraction: 1.2)) {
+                                    HapticManager.instance.impact(style: .light)
+                                    viewModel.selectedExpandIndex = nil
+                                    
+                                    
+                                    viewModel.showItems = false
+                                    viewModel.moveItems = false
+                                }
+                                
+                            } ,label: {
+                                
+                                Rectangle()
+                                
+                                    .foregroundStyle(.accent)
+                                
+                                    .frame(width: 300, height: 56, alignment: .center)
+                                    .clipShape(RoundedRectangle(cornerRadius: 40, style: .continuous))
+                                    .overlay(
+                                        Text("Confirm")
+                                            .foregroundColor(.black) // Set the text color
+                                            .bold()
+                                    )
+                                    .padding(48)
+                            })
+                            .buttonStyle(BouncyButton())
+                            
+                        }
+                        
+                        .scaleEffect(appear ? 1 : 0.5, anchor: .bottom)
+                        .opacity(appear ? 1 : 0)
+                        .transition(.scale(scale: 1))
+                        .padding(.bottom, 50)
+                    }
+                    .fontDesign(.rounded)
+                    .background(
+                        RoundedRectangle(cornerRadius: 24, style: .continuous)
+                            .fill(.gray0)
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask({
+                        RoundedRectangle(cornerRadius: 24, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
+                        
+                    })
+                    .ignoresSafeArea(.all)
+                    
+                    .onAppear(perform: {
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.85, blendDuration: 1)) {
+                            appear = true
+                        }
+                    })
+                }
+            }        }
     }
-}
+
+    struct RecipeDetailView: View {
+        let recipe: Recipe
+
+        var body: some View {
+            VStack {
+                AsyncImage(url: URL(string: recipe.image)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                Text(recipe.label)
+                    .font(.largeTitle)
+                    .padding()
+
+                Text("Source: \(recipe.source)")
+                    .font(.subheadline)
+                    .padding()
+
+                Link("View Recipe", destination: URL(string: recipe.url)!)
+                    .font(.title2)
+                    .padding()
+            }
+            .navigationTitle("Recipe Details")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
 
 struct SecondExpandedView_Preview: PreviewProvider {
     @Namespace static var namespace
